fd40488430aea40115264d894d4703e1d8c07a45
Kris Wright <47092930+kris-w@users.noreply.github.com>
2019-02-20 14:26:19 -0500

    Update httpseverywhere.ps1
    fixed whitespace issues, fixed bad variable name

--- httpseverywhere.ps1
+++ httpseverywhere.ps1
@@ -1,138 +1,90 @@
 #------------------------
+#
 # httpseverywhere.ps1
-
-
-#----------------------
 #
-#Enable TLS 1.2 in IIS7
-#
+#------------------------
+
+#------------------------
+# Enable TLS 1.2 in IIS7
+#------------------------
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client"
-
 if (-Not(Test-Path "$key")) {
-    
-	New-Item -Path $Tkey -Force
-
+	New-Item -Path $key -Force
 }
-
 Set-ItemProperty -Path $key -Name "DisabledByDefault" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server"
-
 if (-Not(Test-Path "$key")) {
-    
 	New-Item -Path $key -Force
-
 }
-
 Set-ItemProperty -Path $key -Name "DisabledByDefault" -Type Dword -Value 0
 
-#
+#------------------------
 # Disable Old Versions Of SSL and TLS
-# 
+#------------------------
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\PCT 1.0\Client"
-
 if (-Not(Test-Path "$key")) {
-    
-	New-Item -Path $Tkey -Force
-
+	New-Item -Path $key -Force
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\PCT 1.0\Server"
-
 if (-Not(Test-Path "$key")) {
-    
 	New-Item -Path $key -Force
-
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client"
-
 if (-Not(Test-Path "$key")) {
-    
-	New-Item -Path $Tkey -Force
-
+	New-Item -Path $key -Force
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server"
-
 if (-Not(Test-Path "$key")) {
-    
 	New-Item -Path $key -Force
-
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client"
-
 if (-Not(Test-Path "$key")) {
-    
-	New-Item -Path $Tkey -Force
-
+	New-Item -Path $key -Force
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server"
-
 if (-Not(Test-Path "$key")) {
-    
 	New-Item -Path $key -Force
-
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client"
-
 if (-Not(Test-Path "$key")) {
-    
-	New-Item -Path $Tkey -Force
-
+	New-Item -Path $key -Force
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server"
-
 if (-Not(Test-Path "$key")) {
-    
 	New-Item -Path $key -Force
-
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client"
-
 if (-Not(Test-Path "$key")) {
-    
-	New-Item -Path $Tkey -Force
-
+	New-Item -Path $key -Force
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server"
-
 if (-Not(Test-Path "$key")) {
-    
 	New-Item -Path $key -Force
-
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
 
-#
+#------------------------
 # Disable known-weak ciphers 
-#
-
+#------------------------
 $BaseKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $env:COMPUTERNAME)
 $key='SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128/128'
 $valname="Enabled"
@@ -166,15 +118,10 @@ $SubKey = $BaseKey.OpenSubKey($key,$true)
 $SubKey.SetValue($valname, $valdata, [Microsoft.Win32.RegistryValueKind]::DWORD)
 
 $key = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168"
-
 if (-Not(Test-Path "$key")) {
-    
 	New-Item -Path $key -Force
-
 }
-
 Set-ItemProperty -Path $key -Name "Enabled" -Type Dword -Value 0
-
 
 $BaseKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $env:COMPUTERNAME)
 $key='SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\Triple DES 168/168'
